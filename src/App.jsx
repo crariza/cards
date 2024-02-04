bdc980cbbf9eed9ec4b1a18ccdb3d2610757cdf8
carlos <carlos.ariza124@gmail.com>
2024-02-04 12:20:14 -0500

    Corrigiendo rutas

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ function App() {
   let desc = ["Elon Musk is a visionary entrepreneur and CEO known for founding companies like SpaceX and Tesla. His ambitious pursuits include space exploration, electric vehicles, and renewable energy innovations.","Jeff Bezos is a pioneering entrepreneur and founder of Amazon, transforming e-commerce. As a space enthusiast, he founded Blue Origin. Bezos' business acumen has made him one of the world's wealthiest."]
   return (
     <main>
-      <Card name="Elon Musk" description={desc[0]} color="lightBlue" url="../public/elon.jpg"/>
-      <Card name="Jeff Bezos" description={desc[1]} color="red" url="../public/jeff.jpg"/>
+      <Card name="Elon Musk" description={desc[0]} color="lightBlue" url="elon.jpg"/>
+      <Card name="Jeff Bezos" description={desc[1]} color="red" url="jeff.jpg"/>
     </main>
   )
 }
